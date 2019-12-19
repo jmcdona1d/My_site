c4bc00c7874ebd745a52a25302b2b800878585b0
James McDonald <15jgm4@queensu.ca>
2019-12-18 20:21:37 -0500

    Added styled/css and changed socials to horizontal list

--- Project_Portfolio/src/components/socialIcons.js
+++ Project_Portfolio/src/components/socialIcons.js
@@ -2,14 +2,29 @@ import React from "react"
 import Linkedin from "./icons/Linkedin"
 import GitHub from "./icons/gitHub"
 import Email from "./icons/email"
-import { Row, Col } from "reactstrap"
+import styled from "styled-components"
+
+const List = styled.ul`
+  display:inline
+  background-color:#eeeeee
+`
+
+const ListEl = styled.li`
+  display: inline;
+`
 
 const SocialIcons = () => (
-  <Row>
-    <Col xs lg="2">
-      <Linkedin /> <GitHub /> <Email />
-    </Col>
-  </Row>
+  <List>
+    <ListEl>
+      <Linkedin />
+    </ListEl>
+    <ListEl>
+      <GitHub />
+    </ListEl>
+    <ListEl>
+      <Email />
+    </ListEl>
+  </List>
 )
 
 export default SocialIcons
