142366b196d44c76282add8359ad27627e52e7b9
James McDonald <15jgm4@queensu.ca>
2019-12-19 19:05:32 -0500

    Fixed weird mail logo

--- Project_Portfolio/src/components/icons/email.js
+++ Project_Portfolio/src/components/icons/email.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Icon, InlineIcon } from "@iconify/react"
 import emailOutline from "@iconify/icons-mdi/email-outline"
+import contactMail from "@iconify/icons-mdi/contact-mail"
+import googleGmail from "@iconify/icons-logos/google-gmail"
 
 const Email = () => (
   <a className="icon" href="mailto:james.mcdonald@queensu.ca">
-    <Icon icon={emailOutline} width="28px" />
+    <Icon icon={googleGmail} width="33px" />
   </a>
 )
 
