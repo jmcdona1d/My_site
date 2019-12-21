fd88b6a633e5ecaa3c2760a5179f5bee27bae2f6
James McDonald <15jgm4@queensu.ca>
2019-12-21 17:45:57 -0500

    Added projectDisplay componenet work

---
+++ Project_Portfolio/src/components/projectDisplay.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Card, MuiThemeProvider } from "material-ui"
+
+const ProjectDisplay = () => (
+  <div>
+    <MuiThemeProvider>
+      <Card>
+        <p>This is a fake project</p>
+      </Card>
+    </MuiThemeProvider>
+  </div>
+)
+
+export default ProjectDisplay
--- Project_Portfolio/src/pageSections/projects.js
+++ Project_Portfolio/src/pageSections/projects.js
@@ -1,9 +1,11 @@
 import React from "react"
+import ProjectDisplay from "../components/projectDisplay"
 
 const Projects = () => (
   <div id="projects_section">
     <h2>Project Portfolio</h2>
     <p>These are projects I have worked on</p>
+    <ProjectDisplay />
   </div>
 )
 
