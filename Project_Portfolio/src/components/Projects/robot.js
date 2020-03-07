41d3a5065225ad9002625a536f38fa6c25366e91
James McDonald <15jgm4@queensu.ca>
2020-03-06 19:33:21 -0500

    Added robot project

---
+++ Project_Portfolio/src/components/Projects/robot.js
@@ -0,0 +1,82 @@
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Card from "@material-ui/core/Card"
+import CardActionArea from "@material-ui/core/CardActionArea"
+import CardActions from "@material-ui/core/CardActions"
+import CardContent from "@material-ui/core/CardContent"
+import Button from "@material-ui/core/Button"
+import Typography from "@material-ui/core/Typography"
+import styled from "styled-components"
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 900,
+  },
+  media: {
+    height: 140,
+  },
+})
+
+const ProjectDescription = styled.div`
+  width: 60%
+  margin-right:2%;
+  float: left;
+`
+
+const ProjectDemo = styled.div`
+  width: 35%;
+  float: left;
+`
+
+const Iframe = styled.div`
+  width: 100%;
+`
+const ClearFix = styled.div`
+  clear: both;
+`
+
+export default function Robot() {
+  const classes = useStyles()
+
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardActionArea style={{ backgroundColor: "#D1E8E2" }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Ball Sorting Robot
+            </Typography>
+            <ProjectDescription>
+              <Typography variant="body2" color="textSecondary" component="p">
+                <ul>
+                  <li>
+                    group project where we programmed a robot to retrieve balls
+                    and sort them into various pipes based on a received{" "}
+                    <em>IR</em>
+                    signal
+                  </li>
+                  <li>
+                    Developed robot logic using <strong>C</strong> and{" "}
+                    <strong>Arduino IDE</strong>
+                  </li>
+
+                  <li>
+                    Created for the <em>Mechatronics Project</em> course
+                  </li>
+                  <li>
+                    Team was one of the top 3 finishers in the end-of-term
+                    competition
+                  </li>
+                </ul>
+                <p></p>
+              </Typography>
+            </ProjectDescription>
+            <Iframe />
+            <ProjectDemo></ProjectDemo>
+          </CardContent>
+        </CardActionArea>
+        <ClearFix />
+      </Card>
+    </div>
+  )
+}
--- Project_Portfolio/src/pageSections/projects.js
+++ Project_Portfolio/src/pageSections/projects.js
@@ -9,6 +9,7 @@ import OpenSource from "../components/Projects/openSource"
 import Databases from "../components/Projects/databases"
 import Solitaire from "../components/Projects/solitaire"
 import FiscalFresh from "../components/Projects/fiscalFresh"
+import Robto from "../components/Projects/robot"
 
 import styled from "styled-components"
 
@@ -56,6 +57,9 @@ const Projects = () => (
     <h4>Academic Projects</h4>
     <ProjectList>
       <Databases />
+    </ProjectList>
+    <ProjectList>
+      <Robot />
     </ProjectList>
     <ProjectList>
       <Solitaire />
